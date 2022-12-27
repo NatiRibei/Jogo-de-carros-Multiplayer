f87458bdc1004bf2ae35466527ae62a40a1c395e
Natalia Oliveira Ribeiro <91141994+NatiRibei@users.noreply.github.com>
2022-12-27 20:24:17 -0300

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,66 @@
+class Game {
+  constructor() {}
+
+  getState(){
+
+    //defina as funções da classe
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+
+
+}
+
+  
+
+  update(state){
+    
+    //defina as funções da classe
+    database.ref("/").update({
+      gameState: state
+    });
+
+  }
+
+  start() {
+    
+    // crie uma instância de novo jogador
+    player = new Player ();
+   
+    // inicie a variável playerCount
+    playerCount = player.getCount();
+    
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite (width/2 - 50, height - 100);
+    car1.addImage("car1",car1_img);
+    car1.scale = 0.07;
+
+    // siga o exemplo acima para criar o sprite de car2
+    car2 = createSprite (width/2 - 50, height - 100);
+    car2.addImage("car2",car2_img);
+    car2.scale = 0.07;
+
+    // atribua os objetos ao vetor cars
+    cars = [car1,car2];
+    
+
+  }
+
+  handleElements(){
+    // adicione os estilos à imagem do título.
+  }
+
+  play () {
+
+    //chame a função para esconder os elementos
+    this.handleElements();
+
+    //chame a função para criar os sprites
+    drawSprites();
+  
+  }
+}
